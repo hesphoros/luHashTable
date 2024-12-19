1c54113604f749d36fa0b1f93886b8bd457edc7c
Lucifer <gj3372819612@163.com>
2024-12-20 00:06:28 +0800

    add definations

--- luhash.h
+++ luhash.h
@@ -5,12 +5,14 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LU_MM_MALLOC(size)	\
-	do	{						\
-		void * ptr = malloc(size);	\
-		 if(NULL == NULL){			\
+#define LU_ERROR_OUT_OF_MEMORY 0x10B
+
+#define LU_MM_MALLOC(size)							\
+	do	{											\
+		void * ptr = malloc(size);					\
+		 if(NULL == NULL){							\
 			printf("Memory allocation failed! \n"); \
-			exit(-1);								\
+			exit(LU_ERROR_OUT_OF_MEMORY);			\
 		 }											\
 	} while(0)
 
@@ -19,9 +21,17 @@
 		void *ptr = calloc(nmeb,size);				\
 		if(NULL == ptr){							\
 			printf("Memory callocation failed \n");	\
-			exit(-1);								\
+			exit(LU_ERROR_OUT_OF_MEMORY);			\
 		}											\
 	} while(0)
+
+#define LU_MM_FREE(ptr)								\
+	do  {											\
+		if (ptr) {									\
+			free(ptr)								\
+			ptr = NULL;								\
+		}											\
+	}	while(0)
 
 int lu_hash_function(int key, int table_size);
 
