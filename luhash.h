55509c62fb71e5019015ce0556c1f81965e81b0e
Lucifer <gj3372819612@163.com>
2024-12-19 23:44:37 +0800

    update

--- luhash.h
+++ luhash.h
@@ -5,6 +5,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define LU_MM_MALLOC(size)	\
+do	{						\
+	void * ptr = malloc(size);	\
+	 if(NULL == NULL){			\
+		printf("Memory allocation failed! \n"); \
+		exit(1);								\
+	 }											\
+	} while(0)
+
 int lu_hash_function(int key, int table_size);
 
 #endif /** LU_LU_HASH_TABLE_INCLUDE_H_*/
