98bd7d4747316747b37ac090692272187bc4ec28
Lucifer <gj3372819612@163.com>
2024-12-20 21:17:31 +0800

    update

--- luhash.c
+++ luhash.c
@@ -30,6 +30,20 @@ int lu_hash_function(int key, int table_size)
 	return hash % table_size;
 }
 
+/**
+ * Initializes a hash table with the specified number of buckets.
+ * If the specified `table_size` is invalid (less than or equal to 0),
+ * a default size is used instead (`LU_HASH_TABLE_DEFAULT_SIZE`).
+ *
+ * Memory is allocated for the hash table structure and the buckets.
+ * Each bucket is initialized as a linked list by default.
+ *
+ * @param table_size The number of buckets in the hash table. If <= 0, default size is used.
+ * @return A pointer to the newly initialized hash table, or exits the program if memory allocation fails.
+ *
+ * Usage example:
+ *     lu_hash_table_t* hash_table = lu_hash_table_init(16); // Creates a hash table with 16 buckets.
+ */
 lu_hash_table_t* lu_hash_table_init(int table_size)
 {
 	if (table_size <= 0) {
@@ -46,4 +60,34 @@ lu_hash_table_t* lu_hash_table_init(int table_size)
 	}
 
 	return table;
+}
+
+void lu_hash_table_insert(lu_hash_table_t* table, int key, void* value)
+{
+	int index = lu_hash_function(key, table->table_size);
+	lu_hash_bucket_t* bucket = &table->buckets[index];
+	if (LU_HASH_BUCKET_LIST == bucket->bucket_type) {
+		lu_hash_bucket_node_ptr_t new_node = (lu_hash_bucket_node_ptr_t)LU_MM_MALLOC(sizeof(lu_hash_bucket_node_t));
+		if (!new_node) {
+#ifdef LU_HASH_DEBUG
+			printf("Memory allocation  failed for new code\n");
+#endif // LU_HASH_DEBUG
+			return;
+		}
+
+		// Check if the key already exists and update the value
+		lu_hash_bucket_node_t* current = bucket->data.list_head;
+		while (current) {
+			if (current->key == key) {
+				current->value = value; // Update value if key exists
+				return;
+			}
+			current = current->next;
+		}
+
+		//TODO: 如果允许覆盖，则需要在插入前检查并更新已有节点的值，而不是直接插入
+		new_node->value = value;
+		new_node->key = key;
+		new_node->next = bucket->data.list_head;
+	}
 }
